2f576f5b80970aa6997b242eae122ddc7b2cbaf1
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-02-09 16:23:31 +0100

    add mandelbulb initial code

---
+++ codingtrain-mandelbulb/sketch.ts
@@ -0,0 +1,36 @@
+import p5 from 'p5';
+
+let sketch = function (p: p5) {
+    let cubeLength = 32;
+    const setCanvasSize = function () {
+        let factorSize = 0.9;
+        let squareLength = (p.windowWidth < p.windowHeight ? p.windowWidth : p.windowHeight) * factorSize;
+        squareLength = Math.floor(squareLength);
+        p.resizeCanvas(squareLength, squareLength);
+    }
+
+    p.setup = function () {
+        p.createCanvas(500, 500, p.WEBGL);
+        setCanvasSize();
+        p.frameRate(1);
+        p.background(0);
+        for (let i = 0; i < cubeLength; i++) {
+            for (let j = 0; j < cubeLength; j++) {
+                for (let k = 0; k < cubeLength; k++) {
+                    let x = p.map(i, 0, cubeLength, -100, 100);
+                    let y = p.map(j, 0, cubeLength, -100, 100);
+                    let z = p.map(k, 0, cubeLength, -100, 100);
+                    p.stroke(255);
+                    p.point(x, y, z);
+                }
+            }
+        }
+    }
+    p.draw = function () {
+
+    }
+    p.windowResized = function () {
+        setCanvasSize();
+    }
+}
+let instantiatedSketch = new p5(sketch);
