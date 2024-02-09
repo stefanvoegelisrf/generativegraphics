667fa09fd6a4a16eb216c907c894444d4e332b8a
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-02-09 12:16:37 +0100

    Add gui with settings

--- codingtrain-mandelbrot/README.md
+++ codingtrain-mandelbrot/README.md
@@ -1,3 +1,7 @@
 # The Coding Train - Coding Challenge 21 - Mandelbrot set with p5
 This is based on the following tutorial [youtube.com - Coding Challenge #21: Mandbrot Set with p5.js](https://www.youtube.com/watch?v=6z7GQewK-Ks&ab_channel=TheCodingTrain)
 
+## How to run
+- Install dependencies with `npm i`
+- Start the server with `npm run dev`
+- Open on the url mentioned in the terminal e.g. `http://localhost:5173/`
--- codingtrain-mandelbrot/sketch.ts
+++ codingtrain-mandelbrot/sketch.ts
@@ -1,35 +1,49 @@
 import p5 from 'p5';
+import { GUI } from 'lil-gui';
 
 let sketch = function (p: p5) {
+    let settings = {
+        maxIterations: 100,
+        min: -1.5,
+        max: 1.5
+    }
+
     p.setup = function () {
-        p.createCanvas(p.windowWidth, p.windowHeight);
+        let gui = new GUI();
+        p.createCanvas(500, 500);
         p.pixelDensity(1);
+        gui.add(settings, 'maxIterations', 0, 1000);
+        gui.add(settings, 'min', -10, 10);
+        gui.add(settings, 'max', -10, 10);
+
+    }
+    p.draw = function () {
         p.loadPixels();
-        let maxIterations = 100;
+
         for (let x = 0; x < p.width; x++) {
             for (let y = 0; y < p.height; y++) {
                 // Get the current coordinates and map them to the complex plane
-                let a = p.map(x, 0, p.width, -2.5, 2.5);
-                let b = p.map(y, 0, p.height, -2.5, 2.5);
+                let a = p.map(x, 0, p.width, settings.min, settings.max);
+                let b = p.map(y, 0, p.height, settings.min, settings.max);
 
                 let iterations = 0;
                 let ca = a;
                 let cb = b;
 
-                while (iterations < maxIterations) {
+                while (iterations < settings.maxIterations) {
                     let aa = a * a - b * b;
                     let bb = 2 * a * b;
                     a = aa + ca;
                     b = bb + cb;
-                    if (a + b > 16) {
+                    if (Math.abs(a + b) > 16) {
                         break;
                     }
                     iterations++;
                 }
 
-                var brightness = p.map(iterations, 0, maxIterations, 0, 255);
-
-                if(iterations === maxIterations) {
+                var brightness = p.map(iterations, 0, settings.maxIterations, 0, 1);
+                brightness = p.map(Math.sqrt(brightness), 0, 1, 0, 255);
+                if (iterations === settings.maxIterations) {
                     brightness = 0;
                 }
 
