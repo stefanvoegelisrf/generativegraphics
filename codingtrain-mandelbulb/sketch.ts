d0120994c558e84bea408679688345486356f741
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-02-09 16:28:34 +0100

    add orbitcontrols

--- codingtrain-mandelbulb/sketch.ts
+++ codingtrain-mandelbulb/sketch.ts
@@ -12,22 +12,23 @@ let sketch = function (p: p5) {
     p.setup = function () {
         p.createCanvas(500, 500, p.WEBGL);
         setCanvasSize();
-        p.frameRate(1);
+        p.frameRate(12);
+    }
+    p.draw = function () {
         p.background(0);
+        p.orbitControl(10, 10, 10, { freeRotation: true });
+        p.strokeWeight(.1)
+        p.stroke(255);
         for (let i = 0; i < cubeLength; i++) {
             for (let j = 0; j < cubeLength; j++) {
                 for (let k = 0; k < cubeLength; k++) {
                     let x = p.map(i, 0, cubeLength, -100, 100);
                     let y = p.map(j, 0, cubeLength, -100, 100);
                     let z = p.map(k, 0, cubeLength, -100, 100);
-                    p.stroke(255);
                     p.point(x, y, z);
                 }
             }
         }
-    }
-    p.draw = function () {
-
     }
     p.windowResized = function () {
         setCanvasSize();
