20bd9c5a53aaca7839c5df68183b3751708da494
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-02-09 12:19:09 +0100

    Add comments to code

--- codingtrain-mandelbrot/sketch.ts
+++ codingtrain-mandelbrot/sketch.ts
@@ -27,27 +27,35 @@ let sketch = function (p: p5) {
                 let b = p.map(y, 0, p.height, settings.min, settings.max);
 
                 let iterations = 0;
+                // Save the initial values of a and b
                 let ca = a;
                 let cb = b;
 
                 while (iterations < settings.maxIterations) {
+                    // The Mandelbrot set is defined by the equation z = z^2 + c
                     let aa = a * a - b * b;
                     let bb = 2 * a * b;
+                    // Add the initial values of a and b
                     a = aa + ca;
                     b = bb + cb;
+                    // If the result is greater than 16, we break
                     if (Math.abs(a + b) > 16) {
                         break;
                     }
                     iterations++;
                 }
 
+                // Map the number of iterations to a brightness value
                 var brightness = p.map(iterations, 0, settings.maxIterations, 0, 1);
+                // Use the brightness value to set a pixel color
                 brightness = p.map(Math.sqrt(brightness), 0, 1, 0, 255);
                 if (iterations === settings.maxIterations) {
                     brightness = 0;
                 }
 
+                // Get the pixel index
                 let pix = (x + y * p.width) * 4;
+                // Set the pixel color
                 p.pixels[pix + 0] = brightness;
                 p.pixels[pix + 1] = brightness;
                 p.pixels[pix + 2] = brightness;
