45858fa3ea814778bf7bef6281774e68065d55c5
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-02-24 15:53:10 +0100

    add initial points, add d3, add voronoi explanation

--- codingtrain-weighted-voronoi/README.md
+++ codingtrain-weighted-voronoi/README.md
@@ -1,9 +1,14 @@
 # Weighted Voronoi Stippling
 This sketch is based on the following tutorial [youtube.com - Coding Challenge 181: Weighted Voronoi Stippling](https://www.youtube.com/watch?v=Bxdt6T_1qgc&ab_channel=TheCodingTrain)
 
-## What is a voronoi?
-
 ## What is delaunay triangulation?
 With the delaunay triangulation, we are connecting a set of points with triangles. The triangles are drawn in a way, that when the points of the triangle lie on a circle, no other point is inside the circle.
 
 ![Image showing the difference when triangles connected according to delaunay and not according to delaunay](images/delaunay-not-delaunay.svg)
+
+## What is a voronoi?
+A voronoi is the center of the circle that is drawn when doing the delaunay triangulation.
+
+## Relaxing a voronoi
+
+## Using d3 to help
--- codingtrain-weighted-voronoi/sketch.ts
+++ codingtrain-weighted-voronoi/sketch.ts
@@ -1,11 +1,21 @@
-import p5 from 'p5';
+import p5, { Vector } from 'p5';
 
 let sketch = function (p: p5) {
+    let randomPoints: p5.Vector[];
+    let amountOfPoints = 100;
+    let previousSquareLength = 0;
     const setCanvasSize = function () {
         let factorSize = 0.9;
         let squareLength = (p.windowWidth < p.windowHeight ? p.windowWidth : p.windowHeight) * factorSize;
         squareLength = Math.floor(squareLength);
+        if (squareLength === previousSquareLength)
+            return;
+        randomPoints = new Array(amountOfPoints);
+        for (let i = 0; i < amountOfPoints; i++) {
+            randomPoints[i] = p.createVector(p.random(0, squareLength), p.random(0, squareLength));
+        }
         p.resizeCanvas(squareLength, squareLength);
+        previousSquareLength = squareLength;
     }
 
     p.setup = function () {
@@ -13,6 +23,12 @@ let sketch = function (p: p5) {
         setCanvasSize();
     }
     p.draw = function () {
+        p.background(255);
+        p.stroke(0);
+        p.strokeWeight(4);
+        for (let i = 0; i < randomPoints.length; i++) {
+            p.point(randomPoints[i].x, randomPoints[i].y);
+        }
     }
     p.windowResized = function () {
         setCanvasSize();
