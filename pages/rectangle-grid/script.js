4843804058d6af84b55ef0bb0bca9f97d2c93625
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-05-15 20:43:26 +0200

    add rectangle grid with animation

---
+++ pages/rectangle-grid/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", contentLoaded);
+let startTime = new Date();
+function contentLoaded() {
+    const rectangleGrid = document.getElementById("rectangle-grid");
+    for (let i = 0; i < 100; i++) {
+        const rectangle = document.createElement("div");
+        rectangle.classList.add("rectangle");
+        rectangleGrid.appendChild(rectangle);
+    }
+    requestAnimationFrame(animate)
+}
+
+function animate() {
+    let millis = new Date().getTime() - startTime.getTime();
+    console.log(millis)
+    const rectangles = document.getElementsByClassName("rectangle");
+    for (let i = 0; i < rectangles.length; i++) {
+        let element = rectangles[i];
+        if (i % 3 === 0 && millis % 1000 < 500) {
+            element.style.width = "50%";
+        }
+        else {
+            element.style.width = null;
+        }
+
+        if (i % 6 === 0 && millis % 2000 < 300) {
+            element.style.height = "30%";
+        }
+        else {
+            element.style.height = null;
+        }
+    }
+    requestAnimationFrame(animate);
+}
