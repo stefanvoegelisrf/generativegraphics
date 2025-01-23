8fab0b566aff4a16b1604556126397674dfe97a5
stefanvoegelisrf <stefan.voegeli@srf.ch>
2025-01-23 20:40:59 +0100

    update spreading lines

--- pages/spreading-lines/sketch.ts
+++ pages/spreading-lines/sketch.ts
@@ -1,20 +1,32 @@
+import GUI from 'lil-gui';
 import p5 from 'p5';
 
 let sketch = function (p5Library: p5) {
-
+    let gui: GUI;
+    let settings = {
+        backgroundColor: { r: 0, g: 0, b: 0 },
+        backgroundOpacity: 30,
+        amountOfLines: 30,
+        spacingMultiplier: .5,
+        speed: 1,
+        red: 255,
+        green: 0,
+        blue: 255,
+        sinMultiplier: 1,
+        strokeOpacity: 100
+    }
 
     p5Library.setup = function () {
         p5Library.createCanvas(p5Library.windowWidth, p5Library.windowHeight);
+        p5Library.angleMode(p5Library.DEGREES);
+        addGui();
     }
 
     p5Library.draw = function () {
-        p5Library.background(0,0,0,30);
-        let amountOfLines = 10;
-        let timeValue = p5Library.millis() * 0.01;
-        drawSpreadingLines(50, timeValue, 5, 1);
-        drawSpreadingLines(50, timeValue, 1, 5);
-        drawSpreadingLines(50, timeValue, 5, 1, false);
-        drawSpreadingLines(50, timeValue, 1, 5, false);
+        p5Library.background(settings.backgroundColor.r, settings.backgroundColor.g, settings.backgroundColor.b, settings.backgroundOpacity);
+        let timeValue = p5Library.millis() * 0.01 * settings.speed;
+        drawSpreadingLines(settings.amountOfLines, timeValue);
+        drawSpreadingLines(settings.amountOfLines, timeValue);
     }
     p5Library.windowResized = function () {
         p5Library.resizeCanvas(p5Library.windowWidth, p5Library.windowHeight);
@@ -23,48 +35,86 @@ let sketch = function (p5Library: p5) {
     function drawSpreadingLines(
         amountOfLines: number,
         timeValue: number,
-        bottomSpacingMultiplier = 1,
-        topSpacingMultiplier = 1,
-        vertical = true
     ) {
-        for (let i = 0; i < amountOfLines; i++) {
+        for (let i = amountOfLines; i > 0; i--) {
             let sinValue = Math.sin(timeValue + i) * 100;
             p5Library.strokeWeight(i * i * 0.5);
-            p5Library.stroke(255 - i * i * 0.25, 0, 255);
-            let spacing = (i + 2) * i * i;
+            p5Library.stroke(p5Library.map(i, amountOfLines, 0, 0, settings.red), settings.green, p5Library.map(i, 0, amountOfLines, 0, settings.blue), settings.strokeOpacity);
+            let spacing = (i + 2) * i * i * settings.spacingMultiplier;
             // right lines
-            if (vertical) {
-                p5Library.line(
-                    p5Library.windowWidth * 0.5 + sinValue + spacing * topSpacingMultiplier,
-                    0,
-                    p5Library.windowWidth * 0.5 + spacing * bottomSpacingMultiplier,
-                    p5Library.height
-                );
+            p5Library.line(
+                p5Library.windowWidth * 0.5 + sinValue * settings.sinMultiplier + spacing,
+                0,
+                p5Library.windowWidth * 0.5 + spacing,
+                p5Library.height
+            );
 
-                // left lines
-                p5Library.line(
-                    p5Library.windowWidth * 0.5 - sinValue - spacing * topSpacingMultiplier,
-                    0,
-                    p5Library.windowWidth * 0.5 - spacing * bottomSpacingMultiplier,
-                    p5Library.height
-                );
-            } else {
-                p5Library.line(
-                    0,
-                    p5Library.windowHeight * 0.5 + sinValue + spacing * topSpacingMultiplier,
-                    p5Library.width,
-                    p5Library.windowHeight * 0.5 + spacing * bottomSpacingMultiplier
-                );
-
-                // left lines
-                p5Library.line(
-                    0,
-                    p5Library.windowHeight * 0.5 - sinValue - spacing * topSpacingMultiplier,
-                    p5Library.width,
-                    p5Library.windowHeight * 0.5 - spacing * bottomSpacingMultiplier
-                );
-            }
+            // left lines
+            p5Library.line(
+                p5Library.windowWidth * 0.5 - sinValue * settings.sinMultiplier - spacing,
+                0,
+                p5Library.windowWidth * 0.5 - spacing,
+                p5Library.height
+            );
         }
+    }
+
+    function addGui() {
+        gui = new GUI();
+        gui.addColor(settings, "backgroundColor", 255)
+            .name("Background");
+        gui.add(settings, "backgroundOpacity")
+            .name("Background opacity")
+            .min(0)
+            .max(255)
+            .step(1);
+
+        gui.add(settings, "amountOfLines")
+            .name("Amount of lines")
+            .min(1)
+            .max(100)
+            .step(1);
+
+        gui.add(settings, "spacingMultiplier")
+            .name("Spacing multiplier")
+            .min(0.01)
+            .max(2)
+            .step(0.01);
+
+        gui.add(settings, "speed")
+            .name("Speed")
+            .min(0.01)
+            .max(1.5)
+            .step(0.01);
+        gui.add(settings, "sinMultiplier")
+            .name("Sin multiplier")
+            .min(0.1)
+            .max(100)
+            .step(.01);
+        gui.add(settings, "strokeOpacity")
+            .name("Stroke opacity")
+            .min(0)
+            .max(255)
+            .step(1);
+
+        let colorFolder = gui.addFolder("Color");
+        colorFolder.add(settings, "red")
+            .name("Red")
+            .min(0)
+            .max(255)
+            .step(1);
+
+        colorFolder.add(settings, "green")
+            .name("Green")
+            .min(0)
+            .max(255)
+            .step(1);
+
+        colorFolder.add(settings, "blue")
+            .name("Blue")
+            .min(0)
+            .max(255)
+            .step(1);
     }
 }
 let instantiatedSketch = new p5(sketch);
