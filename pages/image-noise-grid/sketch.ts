cc6a8c86d3564084ddde340e416b91c6f45eb4be
stefanvoegelisrf <stefan.voegeli@srf.ch>
2025-02-26 11:48:48 +0100

    Update sketch to transition between multiple images

--- pages/image-noise-grid/sketch.ts
+++ pages/image-noise-grid/sketch.ts
@@ -1,206 +1,273 @@
 import p5 from 'p5';
 import GUI from 'lil-gui';
 
+class DisplayImage {
+  width: number;
+  height: number;
+  brightnessValues: Array<Array<number>>;
+  private imageReference: p5.Image;
+  constructor(width: number, height: number, imageReference: p5.Image) {
+    this.width = width;
+    this.height = height;
+    this.imageReference = imageReference;
+  }
+  loadPixels() {
+    this.imageReference.loadPixels();
+    return this.imageReference.pixels;
+  }
+}
+
 let sketch = function (p5Library: p5) {
-    let gui: GUI;
-    let settings = {
-        columns: 100,
-        rows: 100,
-        shapeType: "square",
-        blendModeType: p5Library.BLEND,
-        cellSizeMultiplier: 3,
-        movementIntensity: 0.5,
-        sizeNoiseMultiplier: 1,
-        movementNoiseMultiplier: 2.7,
-        fillOrStroke: "stroke",
-        fillOpacity: 20,
-        strokeOpacity: 100,
-        enableRotation: false,
-        backgroundColor: [0, 0, 0],
-        backgroundOpacity: 5,
-    }
-    let img: p5.Image;
-    let brightnessValues = [];
+  let gui: GUI;
+  let settings = {
+    columns: 100,
+    rows: 100,
+    shapeType: "circle",
+    blendModeType: p5Library.BLEND,
+    cellSizeMultiplier: 3,
+    movementIntensity: 0.5,
+    sizeNoiseMultiplier: 1,
+    movementNoiseMultiplier: 2.7,
+    fillOrStroke: "fill",
+    fillOpacity: 20,
+    strokeOpacity: 100,
+    enableRotation: false,
+    backgroundColor: [255, 255, 255],
+    backgroundOpacity: 5,
+    toggleImageChangeInterval: toggleImageChangeInterval,
+    imageChangeIntervalInMs: 5000
+  }
+  let images: Array<DisplayImage> = [];
+  let currentImage: DisplayImage;
+  let currentImageIndex = 0;
+  let isCalculatingBrightness = false;
+  let imageChangeInterval: number | null = null;
 
-    p5Library.preload = function () {
-        img = p5Library.loadImage('./stef.jpg');
-    }
+  const imagePaths = [
+    "./Ankol.png",
+    "./Sagom.png",
+    "./Chiakla.png",
+    "./Sukrup.png",
+  ]
 
-    p5Library.setup = function () {
-        p5Library.createCanvas(p5Library.windowWidth, p5Library.windowHeight);
-        addGui();
-        document.addEventListener("keypress", (event: KeyboardEvent) => {
-            if (event.key === "g") {
-                hideGui();
-            }
-        })
-        p5Library.rectMode(p5Library.CENTER);
-        setFillOrStroke();
-        calculateBrightness();
+  p5Library.preload = function () {
+    for (let imagePath of imagePaths) {
+      p5Library.loadImage(imagePath, (loadedImage) => {
+        images.push(new DisplayImage(loadedImage.width, loadedImage.height, loadedImage));
+      });
     }
-    p5Library.draw = function () {
-        p5Library.background(
-            settings.backgroundColor[0],
-            settings.backgroundColor[1],
-            settings.backgroundColor[2],
-            settings.backgroundOpacity * 0.01 * 255
-          );
-        
-          // Calculate aspect ratio
-          let imgAspectRatio = img.width / img.height;
-          let canvasAspectRatio = p5Library.width / p5Library.height;
-        
-          let gridWidth, gridHeight;
-          if (canvasAspectRatio > imgAspectRatio) {
-            gridHeight = p5Library.height;
-            gridWidth = p5Library.height * imgAspectRatio;
-          } else {
-            gridWidth = p5Library.width;
-            gridHeight = p5Library.width / imgAspectRatio;
-          }
-        
-          let offsetX = (p5Library.width - gridWidth) / 2;
-          let offsetY = (p5Library.height - gridHeight) / 2;
-        
-          let cellWidth = gridWidth / settings.columns;
-          let cellHeight = gridHeight / settings.rows;
-        
-          for (let i = 0; i < settings.columns; i++) {
-            for (let j = 0; j < settings.rows; j++) {
-              let x =
-                offsetX +
-                i * cellWidth +
-                (p5Library.noise(
-                  i * 0.1,
-                  j * 0.1,
-                  p5Library.frameCount * 0.01 * settings.movementNoiseMultiplier
-                ) -
-                  0.5) *
-                  cellWidth *
-                  settings.movementIntensity;
-              let y =
-                offsetY +
-                j * cellHeight +
-                (p5Library.noise(i * 0.1 + 100, j * 0.1 + 100, p5Library.frameCount * 0.01) - 0.5) *
-                  cellHeight *
-                  settings.movementIntensity *
-                  settings.movementNoiseMultiplier;
-              let cellSize =
-                brightnessValues[i][j] *
-                p5Library.min(cellWidth, cellHeight) *
-                settings.cellSizeMultiplier;
-        
-              // Get the color from the image
-              let imgX = p5Library.floor(i * (img.width / settings.columns));
-              let imgY = p5Library.floor(j * (img.height / settings.rows));
-              let imgIndex = 4 * (imgY * img.width + imgX);
-              let r = img.pixels[imgIndex];
-              let g = img.pixels[imgIndex + 1];
-              let b = img.pixels[imgIndex + 2];
-              let col = p5Library.color(r, g, b, settings.fillOpacity * 0.01 * 255);
-              let strokeCol = p5Library.color(r, g, b, settings.strokeOpacity * 0.01 * 255);
-        
-              if (settings.fillOrStroke === "fill") {
-                p5Library.fill(col);
-              } else {
-                p5Library.stroke(strokeCol);
-              }
-        
-              p5Library.push();
-              p5Library.translate(x, y);
-              if (settings.enableRotation) {
-                p5Library.rotate(p5Library.noise(i * 0.1, j * 0.1, p5Library.frameCount * 0.01) * p5Library.TWO_PI);
-              }
-              if (settings.shapeType === "square") {
-                p5Library.rectMode(p5Library.CENTER);
-                p5Library.rect(0, 0, cellSize, cellSize);
-              } else {
-                p5Library.ellipse(0, 0, cellSize, cellSize);
-              }
-              p5Library.pop();
-            }
-          }
+  }
+
+  p5Library.setup = function () {
+    p5Library.createCanvas(p5Library.windowWidth, p5Library.windowHeight);
+    addGui();
+    document.addEventListener("keypress", (event: KeyboardEvent) => {
+      if (event.key === "g") {
+        hideGui();
+      }
+    })
+    p5Library.rectMode(p5Library.CENTER);
+    setFillOrStroke();
+    processImages();
+  }
+  p5Library.draw = function () {
+    p5Library.background(
+      settings.backgroundColor[0],
+      settings.backgroundColor[1],
+      settings.backgroundColor[2],
+      settings.backgroundOpacity * 0.01 * 255
+    );
+    if (isCalculatingBrightness) return;
+
+    // Calculate aspect ratio
+    let imageAspectRatio = currentImage.width / currentImage.height;
+    let canvasAspectRatio = p5Library.width / p5Library.height;
+
+    let gridWidth: number = p5Library.width;
+    let gridHeight: number = p5Library.width / imageAspectRatio;
+    if (canvasAspectRatio > imageAspectRatio) {
+      gridHeight = p5Library.height;
+      gridWidth = p5Library.height * imageAspectRatio;
     }
 
-    function calculateBrightness() {
-        img.loadPixels();
-        let imgWidth = img.width;
-        let imgHeight = img.height;
-        let cellWidth = imgWidth / settings.columns;
-        let cellHeight = imgHeight / settings.rows;
-        brightnessValues = [];
-        for (let i = 0; i < settings.columns; i++) {
-          let col = [];
-          for (let j = 0; j < settings.rows; j++) {
-            let sumBrightness = 0;
-            for (let x = 0; x < cellWidth; x++) {
-              for (let y = 0; y < cellHeight; y++) {
-                let index = 4 * ((j * cellHeight + y) * imgWidth + (i * cellWidth + x));
-                let r = img.pixels[index];
-                let g = img.pixels[index + 1];
-                let b = img.pixels[index + 2];
-                let brightness = (r + g + b) / 3;
-                sumBrightness += brightness;
-              }
-            }
-            let avgBrightness = sumBrightness / (cellWidth * cellHeight);
-            if (isNaN(avgBrightness)) {
-              avgBrightness = 0.01; // Handle NaN values
-            }
-            col.push(p5Library.map(avgBrightness, 0, 255, 0, 1)); // Map brightness to range 0-1
+    let offsetX = (p5Library.width - gridWidth) / 2;
+    let offsetY = (p5Library.height - gridHeight) / 2;
+
+    let cellWidth = gridWidth / settings.columns;
+    let cellHeight = gridHeight / settings.rows;
+
+    for (let i = 0; i < settings.columns; i++) {
+      for (let j = 0; j < settings.rows; j++) {
+        let x =
+          offsetX +
+          i * cellWidth +
+          (p5Library.noise(
+            i * 0.1,
+            j * 0.1,
+            p5Library.frameCount * 0.01 * settings.movementNoiseMultiplier
+          ) -
+            0.5) *
+          cellWidth *
+          settings.movementIntensity;
+        let y =
+          offsetY +
+          j * cellHeight +
+          (p5Library.noise(i * 0.1 + 100, j * 0.1 + 100, p5Library.frameCount * 0.01) - 0.5) *
+          cellHeight *
+          settings.movementIntensity *
+          settings.movementNoiseMultiplier;
+        let cellSize =
+          currentImage.brightnessValues[i][j] *
+          p5Library.min(cellWidth, cellHeight) *
+          settings.cellSizeMultiplier;
+
+        let opacity = (settings.fillOrStroke === "fill" ? settings.fillOpacity : settings.strokeOpacity) * 0.01 * 255;
+        let color = p5Library.color(currentImage.brightnessValues[i][j] * 255, opacity);
+
+        if (settings.fillOrStroke === "fill") {
+          p5Library.fill(color);
+        } else {
+          p5Library.stroke(color);
+        }
+
+        p5Library.push();
+        p5Library.translate(x, y);
+        if (settings.enableRotation) {
+          p5Library.rotate(p5Library.noise(i * 0.1, j * 0.1, p5Library.frameCount * 0.01) * p5Library.TWO_PI);
+        }
+        if (settings.shapeType === "square") {
+          p5Library.rectMode(p5Library.CENTER);
+          p5Library.rect(0, 0, cellSize, cellSize);
+        } else {
+          p5Library.ellipse(0, 0, cellSize, cellSize);
+        }
+        p5Library.pop();
+      }
+    }
+  }
+
+  function processImages() {
+    isCalculatingBrightness = true;
+    for (let image of images) {
+      image.brightnessValues = calculateBrightness(image);
+    }
+    currentImage = images[currentImageIndex];
+    isCalculatingBrightness = false;
+  }
+
+  function calculateBrightness(image: DisplayImage) {
+    let pixels = image.loadPixels();
+    let imgWidth = image.width;
+    let imgHeight = image.height;
+    let cellWidth = imgWidth / settings.columns;
+    let cellHeight = imgHeight / settings.rows;
+    let brightnessValues: Array<Array<number>> = [];
+    let uniqueBrightnessValues = [];
+    for (let i = 0; i < settings.columns; i++) {
+      let column: Array<number> = [];
+      for (let j = 0; j < settings.rows; j++) {
+        let sumBrightness = 0;
+        for (let x = 0; x < cellWidth; x++) {
+          for (let y = 0; y < cellHeight; y++) {
+            let index = 4 * ((j * cellHeight + y) * imgWidth + (i * cellWidth + x));
+            let r = pixels[index];
+            let g = pixels[index + 1];
+            let b = pixels[index + 2];
+            let brightness = (r + g + b) / 3;
+            sumBrightness += brightness;
           }
-          brightnessValues.push(col);
+        }
+        let avgBrightness = sumBrightness / (cellWidth * cellHeight);
+        if (isNaN(avgBrightness)) {
+          avgBrightness = 0.01; // Handle NaN values
+        }
+        let normalizedBrightness = p5Library.map(avgBrightness, 0, 255, 0, 1)
+        column.push(normalizedBrightness);
+        if (uniqueBrightnessValues.find(b => b === normalizedBrightness) === undefined) {
+          uniqueBrightnessValues.push(normalizedBrightness);
         }
       }
-
-    p5Library.windowResized = function () {
-        p5Library.resizeCanvas(p5Library.windowWidth, p5Library.windowHeight);
+      brightnessValues.push(column);
     }
+    return brightnessValues;
+  }
 
-    function hideGui() {
-        gui.show(gui._hidden)
-    }
+  function imageChangeIntervalChanged() {
+    if (imageChangeInterval === null) return;
+    clearInterval(imageChangeInterval);
+    imageChangeInterval = null;
+    toggleImageChangeInterval();
+  }
 
-    function setFillOrStroke() {
-        if (settings.fillOrStroke === "fill") {
-            p5Library.noStroke();
-        } else {
-            p5Library.noFill();
-        }
+  function toggleImageChangeInterval() {
+    if (imageChangeInterval === null) {
+      imageChangeInterval = setInterval(() => {
+        transitionToNextImage();
+      }, settings.imageChangeIntervalInMs);
     }
+    else {
+      clearInterval(imageChangeInterval);
+      imageChangeInterval = null;
+    }
+  }
 
-    let addGui = function () {
-        gui = new GUI();
-        gui.add(settings, "columns").name("Columns").min(1).max(200).step(1).onChange(calculateBrightness);
-        gui.add(settings, "rows").name("Rows").min(1).max(200).step(1).onChange(calculateBrightness);
-        gui.add(settings, "cellSizeMultiplier").name("Cell size multiplier").min(0.5).max(20).step(0.1);
-        gui.add(settings, "movementIntensity").name("Movement intensity").min(0.5).max(20).step(0.1);
-        gui.add(settings, "sizeNoiseMultiplier").name("Size noise multiplier").min(0.5).max(20).step(0.1);
-        gui.add(settings, "movementNoiseMultiplier").name("Movement noise multiplier").min(0.5).max(20).step(0.1);
-        gui.add(settings, "fillOrStroke").name("Fill or stroke").options(["fill", "stroke"])
-            .onChange(() => {
-                setFillOrStroke();
-            });
-        gui.add(settings, "fillOpacity").name("Fill opacity").min(0).max(100).step(1);
-        gui.add(settings, "strokeOpacity").name("Stroke opacity").min(0).max(100).step(1);
-        gui.add(settings, "enableRotation").name("Enable rotation");
-        gui.add(settings, "shapeType").name("Shape type").options(["square", "sphere"]);
-        gui.add(settings, "blendModeType").name("Blend mode").options([
-            p5Library.BLEND,
-            p5Library.ADD,
-            p5Library.DARKEST,
-            p5Library.LIGHTEST,
-            p5Library.DIFFERENCE,
-            p5Library.EXCLUSION,
-            p5Library.MULTIPLY,
-            p5Library.SCREEN,
-            p5Library.REPLACE,
-            p5Library.REMOVE,
-        ]).onChange((blendMode: p5.BLEND_MODE) => {
-            p5Library.blendMode(blendMode)
-        });
-        gui.addColor(settings, "backgroundColor", 100).name("Background color");
-        gui.add(settings, "backgroundOpacity").name("Background opacity").min(0).max(100).step(1);
+  function transitionToNextImage() {
+    currentImageIndex++;
+    if (currentImageIndex > images.length - 1) {
+      currentImageIndex = 0;
     }
+    currentImage = images[currentImageIndex]
+  }
+  p5Library.windowResized = function () {
+    p5Library.resizeCanvas(p5Library.windowWidth, p5Library.windowHeight);
+  }
+
+  function hideGui() {
+    gui.show(gui._hidden)
+  }
+
+  function setFillOrStroke() {
+    if (settings.fillOrStroke === "fill") {
+      p5Library.noStroke();
+    } else {
+      p5Library.noFill();
+    }
+  }
+
+  let addGui = function () {
+    gui = new GUI();
+    gui.add(settings, "columns").name("Columns").min(1).max(200).step(1).onChange(processImages);
+    gui.add(settings, "rows").name("Rows").min(1).max(200).step(1).onChange(processImages);
+    gui.add(settings, "cellSizeMultiplier").name("Cell size multiplier").min(0.5).max(20).step(0.1);
+    gui.add(settings, "movementIntensity").name("Movement intensity").min(0.5).max(20).step(0.1);
+    gui.add(settings, "sizeNoiseMultiplier").name("Size noise multiplier").min(0.5).max(20).step(0.1);
+    gui.add(settings, "movementNoiseMultiplier").name("Movement noise multiplier").min(0.5).max(20).step(0.1);
+    gui.add(settings, "fillOrStroke").name("Fill or stroke").options(["fill", "stroke"])
+      .onChange(() => {
+        setFillOrStroke();
+      });
+    gui.add(settings, "fillOpacity").name("Fill opacity").min(0).max(100).step(1);
+    gui.add(settings, "strokeOpacity").name("Stroke opacity").min(0).max(100).step(1);
+    gui.add(settings, "enableRotation").name("Enable rotation");
+    gui.add(settings, "shapeType").name("Shape type").options(["square", "sphere"]);
+    gui.add(settings, "blendModeType").name("Blend mode").options([
+      p5Library.BLEND,
+      p5Library.ADD,
+      p5Library.DARKEST,
+      p5Library.LIGHTEST,
+      p5Library.DIFFERENCE,
+      p5Library.EXCLUSION,
+      p5Library.MULTIPLY,
+      p5Library.SCREEN,
+      p5Library.REPLACE,
+      p5Library.REMOVE,
+    ]).onChange((blendMode: p5.BLEND_MODE) => {
+      p5Library.blendMode(blendMode)
+    });
+    gui.addColor(settings, "backgroundColor", 100).name("Background color");
+    gui.add(settings, "backgroundOpacity").name("Background opacity").min(0).max(100).step(1);
+    gui.add(settings, "toggleImageChangeInterval").name("Toggle automatic image change");
+    gui.add(settings, "imageChangeIntervalInMs").name("Image change interval in milliseconds").min(500).max(50000).step(100).onChange(imageChangeIntervalChanged);
+  }
 }
 let instantiatedSketch = new p5(sketch);
