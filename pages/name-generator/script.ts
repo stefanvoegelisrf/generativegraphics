9c504ec2bc218637a10cee00b254173defec6db6
stefanvoegelisrf <stefan.voegeli@srf.ch>
2025-05-09 12:06:23 +0200

    update random name generator styling

--- pages/name-generator/script.ts
+++ pages/name-generator/script.ts
@@ -7,7 +7,7 @@ let historyIndex = 1;
 
 function contentLoaded() {
     generateNames();
-    const generateNamesButton = document.getElementById("generateNames")!;
+    const generateNamesButton = document.getElementById("generate-names")!;
     generateNamesButton.addEventListener("click", generateNames);
 }
 
@@ -22,27 +22,29 @@ function generateNames() {
     const amountOfNames = parseInt((<HTMLInputElement>document.getElementById("amountOfNames")).value);
     const minSyllables = parseInt((<HTMLInputElement>document.getElementById("minSyllables")).value)
     const maxSyllables = parseInt((<HTMLInputElement>document.getElementById("maxSyllables")).value)
-    const nameContainer = document.getElementById("nameContainer")!;
-    nameContainer.innerHTML = "";
-    const historyEntries = document.getElementById("historyEntries");
-    const historyEntryContainer = document.createElement("div");
+    const nameList = document.getElementById("name-list")!;
+    nameList.innerHTML = "";
+    const historyEntries = document.getElementById("history-entries");
+    const historyEntryContainer = document.createElement("details");
     historyEntryContainer.classList.add("history-entry");
-    const historyEntryTitle = document.createElement("h2");
+    const historyEntryTitle = document.createElement("summary");
     historyEntryTitle.textContent = `${historyIndex}`;
     historyEntryContainer.appendChild(historyEntryTitle);
+    const historyEntryNameList=document.createElement("div");
+    historyEntryNameList.classList.add("history-entry-name-list");
     console.log(amountOfNames);
     for (let i = 0; i < amountOfNames; i++) {
-        const nameElement = document.createElement("p");
+        const nameElement = document.createElement("button");
         nameElement.classList.add("name-item");
         nameElement.onclick = copyName;
         const randomName = GenerateRandomName(minSyllables, maxSyllables);
         nameElement.textContent = randomName;
-        nameContainer.appendChild(nameElement)
+        nameList.appendChild(nameElement)
         const historyNameElement = nameElement.cloneNode(true) as HTMLElement;
         historyNameElement.onclick = copyName;
-        historyEntryContainer.appendChild(historyNameElement);
+        historyEntryNameList.appendChild(historyNameElement);
     }
-
+    historyEntryContainer.appendChild(historyEntryNameList);
     historyEntries?.appendChild(historyEntryContainer);
     historyIndex++;
 }
